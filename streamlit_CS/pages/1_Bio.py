3199541d240c1c898eed0a5acda73cf5a8ef6848
Brady Reiner <123605565+ydarbreiner@users.noreply.github.com>
2025-10-20 16:20:48 -0600

    Update personal bio information in 1_Bio.py

--- streamlit_CS/pages/1_Bio.py
+++ streamlit_CS/pages/1_Bio.py
@@ -3,18 +3,18 @@ import streamlit as st
 st.title("👋 My Bio")
 
 # ---------- TODO: Replace with your own info ----------
-NAME = "Your Name"
-PROGRAM = "Your Program / Major / Role"
+NAME = "Brady Reiner"
+PROGRAM = "Computer Science"
 INTRO = (
-    "Write 2–3 sentences about yourself: what you’re studying/working on, "
-    "what excites you about data visualization or computing, etc."
+    "Hi! I am finishing up my Bachelors of Science in Computer Science at MSU Denver."
+    "I am very interested and involved in Data Science and Machine Learning. I am interested in Data Visualization because it will help me in undertsanding and working with the data I already collect outside of school."
 )
 FUN_FACTS = [
-    "I love …",
-    "I’m learning …",
-    "I want to build …",
+    "I love being outdoors usually rock climbing, hiking, or skiing.",
+    "I’m learning how to use Computer Science to solve real world problems",
+    "I want to use technology to help people and improve everyones quality of life.",
 ]
-PHOTO_PATH = "your_photo.jpg"  # Put a file in repo root or set a URL
+PHOTO_PATH = "assets/IMG_2929-2.jpg"  # Put a file in repo root or set a URL
 
 # ---------- Layout ----------
 col1, col2 = st.columns([1, 2], vertical_alignment="center")
